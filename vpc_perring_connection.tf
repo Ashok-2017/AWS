874f957a04e2f516f759b81d07dbfc8681335bab
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2022-12-28 18:33:13 +0530

    Update vpc_perring_connection.tf

--- vpc_perring_connection.tf
+++ vpc_perring_connection.tf
@@ -9,4 +9,30 @@ data "aws_caller_identity" "peer1" {
   provider = aws.vikram
 }
 
-resource "
+#Requester side of the connection
+resource "aws_vpc_peering_connection" "peer3" {
+  provider = aws.xxxx
+  vpc_id = tolist(data.aws_vpcs.peer.ids)[0]
+  peer_vpc_id = tolist(data.aws_vpcs.peer.ids)[0]
+  peer_owner_id = tolist(data.aws_caller_identity.peer1.account_id)
+  auto_accept = false
+  
+  tags = {
+    "side" = "requester"
+    "environment" = "staging"
+    "Name" = "xxxxx"
+  }
+}
+
+#Acceptor's side of the connection
+resource "aws_vpc_peering_connection_acceptor" "peer4" {
+  provider = aws.xxxx
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer3.id
+  auto_accept               = true
+
+  tags = {
+    side            = "accepter"
+    "environment"   = "staging"
+    "Name"          = ""
+  }
+}
