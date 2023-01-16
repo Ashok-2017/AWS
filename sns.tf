990b360d5d732ffb77118b20e5f0e9e5ae483d91
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2023-01-16 17:31:49 +0530

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -1,3 +1,17 @@
+
+resource "aws_kms_key" "sns_topics" {
+  description = " testing"
+  tags = {
+    "environment" = "production"
+  }
+  policy = file("./kms_key.json")
+  enable_key_rotation = true
+}
+
+resource "aws_kms_alias" "sns" {
+  target_key_id = aws_kms_key.sns_topics.key_id
+}
+
 resource "aws_sns_topic" "t1" {
   name = var.name
   display = var.display_name
