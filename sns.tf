aa95904ddf7e696436a170ee3c1f8d3754391fde
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2023-01-16 16:24:42 +0530

    Create sns.tf

---
+++ sns.tf
@@ -0,0 +1,23 @@
+resource "aws_sns_topic" "t1" {
+  name = var.name
+  display = var.display_name
+  kms_master_key_id = var.kms_key_id
+  tags = {
+    "environment" = var.environment
+  }
+  
+  resource "aws_sns_topic" "fail" {
+    for_each = local.environments
+    display_name = "failed_${each.key}"
+    tags = { 
+      "environment" = each.value" 
+      }
+  }
+  
+  resource "aws_sns_topic_subscription" "email-1" {
+    for_each = local.environments
+    topic_arn = 
+    protocol = "email"
+    endpoint = "ashokrdy51@gmail.com"
+  }  
+  
