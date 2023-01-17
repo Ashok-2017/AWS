502d650fb7f09a7c2210ea94b66f8fea5aacbcd1
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2023-01-17 15:15:51 +0530

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,8 @@
+resource "aws_lambda_permission" "external" {
+  statement_id = "AllowExecutionFromS3bucket${local.Env}}
+  action = "lambda: Invokefunction" 
+  function_name = data.aws_lambda_function
+  principal = "s3.amazonaws.com"
+  source_arn = 
+}  
+  
